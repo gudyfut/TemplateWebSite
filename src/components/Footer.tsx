d82493eb2967d3162b97abaa4f49863bc66e8a3f
gudyfut <murilorfm@gmail.com>
2025-11-03 20:07:47 -0300

    blue interfac

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,13 @@
-import wave from '../assets/images/wave.svg';
 import '../styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer>
-  <img id="wave" src={wave} alt="Decorativo" />
       <div id="footer_items">
         <span id="copyright">© 2025 Restaurante Delícia</span>
         <div className="social-media-buttons">
           <a href="#"><i className="fa-brands fa-whatsapp"></i></a>
-          <a href="#"><i className="fa-brands fa-instagram"></i></a>
+          <a href="https://www.instagram.com/puhlequipamentos/?hl=pt"><i className="fa-brands fa-instagram"></i></a>
           <a href="#"><i className="fa-brands fa-facebook"></i></a>
         </div>
       </div>
